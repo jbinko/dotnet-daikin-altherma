db59d8b48d35792447f7010d96f9e2fc52bd2e9c
jbinko <jbinko+github@gmail.com>
2022-12-11 22:26:48 +0100

    dotnet-daikin-altherma-sample - init commit

---
+++ samples/dotnet-daikin-altherma-sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using DotNet.Daikin.Altherma;
+
+using var da = new DaikinAltherma();
+await da.ConnectAsync("myDaikinHost");
+
+for (; ; )
+{
+    var info = await da.GetDeviceInfoAsync();
+    Console.WriteLine($"Adapter Model:             {info.AdapterModel}");
+    Console.WriteLine($"Outdoor Temperature:       {info.OutdoorTemperature}");
+    Console.WriteLine($"Indoor Temperature:        {info.IndoorTemperature}");
+    Console.WriteLine($"Leaving Water Temperature: {info.LeavingWaterTemperature}");
+    Console.WriteLine($"Target Temperature:        {info.TargetTemperature}");
+    Console.WriteLine($"Power State:               {info.PowerState}");
+    //await da.SetHeatingAsync(PowerState.On);
+    //await da.SetTargetTemperatureAsync(23);
+
+    System.Threading.Thread.Sleep(30 * 1000); // Report each 30s
+}
